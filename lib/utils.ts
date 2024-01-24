8d2d2eba2aa58fd111725e32f8836d6e8083fcd7
dragonjambo97 <jankiewicz.dawid@gmail.com>
2024-01-24 21:58:41 +0100

    delete allowed things

--- lib/utils.ts
+++ lib/utils.ts
@@ -219,10 +219,6 @@ export const matchesEtcPasswdFile = (text: string) => {
 
 export const sanitizeInput = (input:string) => {
   return sanitizeHtml(input, {  
-    allowedTags: ['b', 'i', 'em', 'strong', 'a', 'ul', 'li', 'p'],
-  allowedAttributes: {
-    'a': ['href']
-  }
   }
     // tutaj można dodać inne opcje konfiguracyjne
   )
