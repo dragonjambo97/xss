42d1fd0bafbd941d1d7a91ad219b464275f8c3f2
dragonjambo97 <jankiewicz.dawid@gmail.com>
2024-01-28 12:51:57 +0100

    CSP

--- server.ts
+++ server.ts
@@ -130,7 +130,7 @@ const startupGauge = new client.Gauge({
 //CSP POLICY
 
 app.use((req: Request, res: Response, next: NextFunction) => {
-  res.setHeader("Content-Security-Policy", "default-src 'self'; script-src 'self'; object-src 'none'; style-src 'self' 'unsafe-inline'; img-src 'self' data:;");
+  res.setHeader("Content-Security-Policy", "default-src 'self'; script-src 'self' https://trusted-source.com; object-src 'none'");
   next();
 });
 
