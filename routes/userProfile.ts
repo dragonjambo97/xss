a1ffafc250878908fbda7fe6db2673b4804ae6eb
dragonjambo97 <jankiewicz.dawid@gmail.com>
2024-01-29 21:24:15 +0100

    routes/userProfile.ts back comment

--- routes/userProfile.ts
+++ routes/userProfile.ts
@@ -26,7 +26,7 @@ module.exports = function getUserProfile () {
         UserModel.findByPk(loggedInUser.data.id).then((user: UserModel | null) => {
           let template = buf.toString()
           let username = user?.username
-          /*if (username?.match(/#{(.*)}/) !== null && !utils.disableOnContainerEnv()) {
+          if (username?.match(/#{(.*)}/) !== null && !utils.disableOnContainerEnv()) {
             req.app.locals.abused_ssti_bug = true
             const code = username?.substring(2, username.length - 1)
             try {
@@ -39,7 +39,7 @@ module.exports = function getUserProfile () {
             }
           } else {
             username = '\\' + username
-          }*/
+          }
           const theme = themes[config.get<string>('application.theme')]
           if (username) {
             template = template.replace(/_username_/g, username)
